d8299b0b68394cb752af18bb0483ed05ad101904
MLMDE <andersgra3@gmail.com>
2024-10-26 15:37:12 +0200

    Add files via upload

---
+++ wdp_01/zadanie3.py
@@ -0,0 +1,19 @@
+przedmiot1 = "\nKognitywistyka pregląd problematyki - wykład\n"
+przedmiot2 = "Kognitywistyka przegląd problematyki - ćwiczenia\n"
+przedmiot3 = "Teoria poznania - ćwiczenia\n"
+przedmiot4 = "Procesy poznawcze: prercepcja i uwaga - wykład\n"
+przedmiot5 = "Wychowanie fizyczne\n"
+przedmiot6 = "Fakultet z języka obcego\n"
+przedmiot7 = "Logika formalna z elementami logicznej teorii języka - wykład\n"
+przedmiot8 = "Logika formalna z elementami logicznej teorii języka - ćwiczenia\n"
+przedmiot9 = "Teoria poznania wykład\n"
+przedmiot10 = "Biologiczne mechanizmy zachowania - ćwiczenia\n"
+przedmiot11 = "Przedsiębiorczość - warsztaty\n"
+przedmiot12 = "Biologiczne mechanizmy zachowania - wykład\n"
+przedmiot13 = "Procesy poznawcze: percepcja i uwaga - ćwiczenia\n"
+przedmiot14 = "Podstawy tutoringu - warsztaty\n"
+przedmiot15 = "Wprowadzenie do programowania - ćwiczenia\n"
+
+print(przedmiot1, przedmiot2, przedmiot3, przedmiot4, przedmiot5, przedmiot6, przedmiot7, przedmiot8, przedmiot9, przedmiot10
+      ,przedmiot10, przedmiot11, przedmiot12, przedmiot13, przedmiot14, przedmiot15)
+
