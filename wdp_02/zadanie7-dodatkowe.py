e5d33849be3be131f301b6561c7b4521706aa960
MLMDE <andersgra3@gmail.com>
2024-10-28 18:52:30 +0100

    zadanie7-dodatkowe.py

---
+++ wdp_02/zadanie7-dodatkowe.py
@@ -0,0 +1,10 @@
+liczba = int(input("wpisz liczbę\n"))
+
+liczba_b = 50
+
+if liczba > 50:
+    print("liczba", liczba, "jest większa niż moja liczba")
+if liczba < 50:
+    print("liczba", liczba, "jest mniejsza niż moja liczba")
+if liczba == 50:
+    print("moja liczba to 50")
