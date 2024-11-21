965605a8568af9d312fb6031c4e6df08660ab435
MLMDE <andersgra3@gmail.com>
2024-11-21 23:43:36 +0100

    Create zadanie10.py

---
+++ wdp_03/zadanie10.py
@@ -0,0 +1,6 @@
+liczba1 = int(input("Wprowadź pierwszą liczbę\n"))
+liczba2 = int(input("Wprowadź drugą liczbę\n"))
+liczba3 = int(input("Wprowadź trzecią liczbę\n"))
+
+if liczba1 % 2 == 0 and liczba2 % 2 == 0 and liczba3 % 2 == 0:
+    print("\nWszystkie liczby są podzielne przez 2.")
