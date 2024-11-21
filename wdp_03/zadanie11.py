76d8dfa6ca49759961e64779fc8c6a252c74f569
MLMDE <andersgra3@gmail.com>
2024-11-21 23:46:58 +0100

    Create zadanie11.py

---
+++ wdp_03/zadanie11.py
@@ -0,0 +1,6 @@
+dane1 = input("Podaj pierwszy ciąg znaków: ")
+dane2 = input("Podaj drugi ciąg znaków: ")
+dane3 = input("Podaj trzeci ciąg znaków: ")
+
+if not (dane1 and dane2 and dane3):
+    print("Co najmniej jeden z ciągów znaków jest pusty.")
