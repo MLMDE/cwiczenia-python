64897af48311fb7829fbb39873b322663fa055ed
MLMDE <andersgra3@gmail.com>
2024-11-21 22:41:35 +0100

    Create zadanie5.py

---
+++ wdp_03/zadanie5.py
@@ -0,0 +1,7 @@
+liczba1 = int(input("Wprowadź liczbę a\n"))
+liczba2 = int(input("Wprowadź liczbę b\n"))
+
+if liczba1 > liczba2:
+    print(liczba2, liczba1)
+elif liczba1 < liczba2:
+    print(liczba1, liczba2)
