f3d0fa5957577ccf5f08b02f80148df3bc50426e
MLMDE <andersgra3@gmail.com>
2024-11-21 22:22:37 +0100

    Create zadanie3.py

---
+++ wdp_03/zadanie3.py
@@ -0,0 +1,8 @@
+liczba = int(input("Wprowadź liczbę:\n"))
+
+if liczba > 0:
+    print("Wprowadzona liczba jest dodatnia.")
+elif liczba == 0:
+    print("Wprowadzona liczba jest zerem.")
+elif liczba < 0:
+    print("Wprowadzona liczba jest ujemna.")
