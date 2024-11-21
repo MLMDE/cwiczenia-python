89ebecfd91b3a8e3aa3eebcbb736c9b4ba7cd394
MLMDE <andersgra3@gmail.com>
2024-11-21 22:15:36 +0100

    Create zadanie2.py

---
+++ wdp_03/zadanie2.py
@@ -0,0 +1,11 @@
+liczba1 = int(input("Wprowadź pierwszą liczbę:\n"))
+liczba2 = int(input("Wprowadź drugą liczbę:\n"))
+liczba3 = int(input("Wprowadź trzecią liczbę:\n"))
+liczba4 = int(input("Wprowadź czwartą liczbę:\n"))
+liczba5 = int(input("Wprowadź piątą liczbę:\n"))
+liczba6 = int(input("Wprowadź szóstą liczbę:\n"))
+
+suma = (liczba1 + liczba2 + liczba3 + liczba4 +liczba5 + liczba6)
+
+if suma >= 30:
+    print("Suma sześciu kolejno wprowadzonych liczb jest większa niż 30.")
