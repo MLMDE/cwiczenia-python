bd63ae623c13c6e5bcf24973284678939a7416a1
MLMDE <andersgra3@gmail.com>
2025-01-01 21:29:37 +0100

    Create zadanie4.py

---
+++ wdp_05/zadanie4.py
@@ -0,0 +1,32 @@
+while True:
+    wybor = int(input("Wprowadź liczbę odpowiadającą miesiącowi:\n"))
+    if 1 <= wybor <= 12:
+        if wybor == 1:
+            print("styczen")
+            break
+        elif wybor == 2:
+            print("luty")
+            break
+        elif wybor == 3:
+            print("marzec")
+            break
+        elif wybor == 4:
+            print("kwiecień")
+        elif wybor == 5:
+            print("maj")
+        elif wybor == 6:
+            print("czerwiec")
+        elif wybor == 7:
+            print("lipiec")
+        elif wybor == 8:
+            print("sierpień")
+        elif wybor == 9:
+            print("wrzesień")
+        elif wybor == 10:
+            print("październik")
+        elif wybor == 11:
+            print("listopad")
+        elif wybor == 12:
+            print("grudzień")
+        break
+       
