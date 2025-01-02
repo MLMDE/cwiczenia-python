d76704ecf8b2476e2727c1fadc965da2c7376cdb
MLMDE <andersgra3@gmail.com>
2025-01-02 15:48:08 +0100

    Update zadanie7.py

--- wdp_05/zadanie7.py
+++ wdp_05/zadanie7.py
@@ -3,3 +3,13 @@ while i <= 20:
     if i % 3 != 0:
         print(i)
     i += 1
+
+
+
+#Prawdopodobnie inaczej zinterpretowałem to zadanie, więc poniżej zamieszczam je w troche innej formie.
+
+i = 1
+while i <= 20:
+    if i % 3 != 0:
+        print(i, end=" ")
+    i += 1
